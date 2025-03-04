885ed0fdfe29443af25e89aa3d9d6d513c8ab921
EmmanuelOZ <davidozuna84@hotmail.com>
2025-03-04 15:49:53 -0600

    eliminar session si el usuario cierra la sesión

--- src/utils/getUserToken.ts
+++ src/utils/getUserToken.ts
@@ -2,13 +2,19 @@ import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
 export async function getUserToken(): Promise<UserProfile> {
-  const result = await cookies();
-
-  const auth_token = result.get("auth_token")?.value ?? "";
+  const auth_token = await getToken();
 
   const user_decode = jwt.decode(auth_token);
 
   const user = user_decode as UserProfile;
 
   return user;
+}
+
+export async function getToken(): Promise<string> {
+  const result = await cookies();
+
+  const auth_token = result.get("auth_token")?.value ?? "";
+
+  return auth_token;
 }
