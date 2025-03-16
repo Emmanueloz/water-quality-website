f48b5ff6241337183ded435ec3000e9c800099b0
Fredev01 <fredev08@gmail.com>
2025-03-15 23:08:23 -0600

    Cambio de query de priv_mod a user_permissions

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -44,13 +44,12 @@ export async function POST(req: NextRequest) {
         r.Rol AS rol, 
         COALESCE(GROUP_CONCAT(m.name SEPARATOR ','), '') AS modules,
         JSON_ARRAYAGG(
-                JSON_OBJECT('idRoute', pm.id_module, 'permissions',  pm.permissions)
+                JSON_OBJECT('idRoute', up.module_id, 'permissions',  up.permissions)
                 ) AS modulesPermissions
       FROM Usuarios u
       INNER JOIN Rol r ON u.Roles = r.id
-      LEFT JOIN privilegios p ON u.id_privilegio = p.id
-      LEFT JOIN priv_mod pm ON p.id = pm.id_privilegio
-      LEFT JOIN modulos m ON pm.id_module = m.id
+      LEFT JOIN user_permissions up ON u.id = up.user_id
+      LEFT JOIN modulos m ON up.module_id = m.id
       WHERE u.Usuario = ?
       GROUP BY u.id, u.Usuario, u.Contraseña, r.Rol;
       `,
@@ -121,7 +120,7 @@ export async function POST(req: NextRequest) {
           user.rol.toLowerCase() === "admin" ? modulesForAdmin : modulesArray,
         modulesPermissions: modulesPermissions,
       },
-      "2m"
+      "20m"
     );
 
     await multiSessionsRepository.create({
